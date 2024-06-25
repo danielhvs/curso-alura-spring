b59256da7b1def96ea5d5fbd577f94d865ae5f6b
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-06-25 11:02:59 -0300

    init

---
+++ springmatch/src/main/java/br/com/alura/springmatch/SpringmatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.springmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmatchApplication.class, args);
+	}
+
+}
---
+++ springmatch/src/test/java/br/com/alura/springmatch/SpringmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.springmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
