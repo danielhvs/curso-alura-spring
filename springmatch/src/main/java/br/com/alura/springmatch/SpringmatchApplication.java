aaadb2358d77be29736efa9e15980db20a164a5f
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-06-25 11:07:25 -0300

    CommandLineRunner

--- springmatch/src/main/java/br/com/alura/springmatch/SpringmatchApplication.java
+++ springmatch/src/main/java/br/com/alura/springmatch/SpringmatchApplication.java
@@ -1,13 +1,22 @@
 package br.com.alura.springmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringmatchApplication {
+public class SpringmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringmatchApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringmatchApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("run");
+    for (String string : args) {
+      System.out.println("arg: " + string);
+    }
+  }
 
 }
