c54bf658aa9646e3c145d1566787e303b2cebe9e
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2024-06-27 08:00:17 -0300

    com proxy

--- springmatch/src/main/java/br/com/alura/springmatch/service/ConsumoApi.java
+++ springmatch/src/main/java/br/com/alura/springmatch/service/ConsumoApi.java
@@ -1,6 +1,9 @@
 package br.com.alura.springmatch.service;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.ProxySelector;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -9,7 +12,8 @@ import java.net.http.HttpResponse;
 public class ConsumoApi {
 
   public String obterDados(String endereco) {
-    HttpClient client = HttpClient.newHttpClient();
+    ProxySelector proxy = ProxySelector.of(new InetSocketAddress("localhost", 3128));
+    HttpClient client = HttpClient.newBuilder().proxy(proxy).build();
     HttpRequest request = HttpRequest.newBuilder()
         .uri(URI.create(endereco))
         .build();
